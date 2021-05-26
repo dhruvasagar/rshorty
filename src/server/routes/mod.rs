4fd77b1a74576940c2c2da454f2c2542980e5780
Dhruva Sagar <dhruva.sagar@gmail.com>
2021-05-26 16:28:24 +0530

    Implement redirection

--- src/server/routes/mod.rs
+++ src/server/routes/mod.rs
@@ -12,6 +12,11 @@ use routerify::{
 };
 use routerify::ext::RequestExt;
 use tracing::{error, info};
+use tokio::sync::mpsc::Sender;
+use crate::{
+    db::DBMessage,
+    models::UrlMapModel,
+};
 
 mod api;
 
@@ -19,6 +24,7 @@ pub fn router() -> RouterBuilder<Body, Error> {
     Router::builder()
         .middleware(Middleware::pre(logger_middleware))
         .get("/", home_get)
+        .get("/:key", redirect)
         .scope("/api", api::router())
         .err_handler_with_info(error_handler)
 }
@@ -35,6 +41,34 @@ async fn logger_middleware(req: Request<Body>) -> Result<Request<Body>, Error> {
 
 async fn home_get(_: Request<Body>) -> Result<Response<Body>, Error> {
     Ok(Response::new(Body::from("rshorty!")))
+}
+
+async fn redirect(req: Request<Body>) -> Result<Response<Body>, Error> {
+    let (tx, rx) = tokio::sync::oneshot::channel();
+    let sender = req.data::<Sender<DBMessage>>().unwrap();
+    let key = req.param("key").unwrap().to_string();
+    sender_failed!(
+        sender
+        .send(DBMessage::GetUrlMap {
+            key: key.clone(),
+            resp: tx,
+        })
+        .await, "GetUrlMap");
+    let url_map = rx.await.unwrap();
+    let url_map = match url_map {
+        Ok(u) => u,
+        Err(_) => {
+            UrlMapModel::new(key, "/".to_string())
+        }
+    };
+    match Response::builder()
+        .header(hyper::header::LOCATION, url_map.url)
+        .status(hyper::StatusCode::SEE_OTHER)
+        .body(Body::from("redirecting..."))
+        {
+            Ok(x) => Ok(x),
+            Err(e) => Err(routerify::Error::new(e.to_string())),
+        }
 }
 
 async fn error_handler(err: routerify::RouteError, _: RequestInfo) -> Response<Body> {
